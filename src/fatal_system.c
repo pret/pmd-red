b81c54d2ce06ca42660a9f62d82cd45b01706baf
Seth Barberee <seth.barberee@gmail.com>
2020-11-02 11:19:59 -0600

    comment unused funcs

--- src/fatal_system.c
+++ src/fatal_system.c
@@ -12,16 +12,19 @@ u32 sub_8011BA4(void)
     return 0;
 }
 
+// Unused
 u32 sub_8011BA8(void)
 {
     return 0;
 }
 
+// Unused
 const char *sub_8011BAC(void)
 {
     return &gNotMountText;
 }
 
+// Unused
 void sub_8011BB4(void)
 {
     Hang();
