c6cef4c839f7c87da9fa90cf0e51249fccac06d5
Seth Barberee <seth.barberee@gmail.com>
2020-11-03 14:03:13 -0600

    start converting personality descriptions

--- src/personality_test.c
+++ src/personality_test.c
@@ -1,6 +1,19 @@
 #include "global.h"
 #include "constants/species.h"
 
+// TODO convert this... maybe a script?
+const u8 gUnknown_80F4244[32] = 
+{
+    0, 0, 0, 0,
+    5, 0, 0, 0,
+    0xC, 0, 6, 0,
+    5, 0, 5, 0,
+    5, 0, 0, 0,
+    0, 0, 0, 0,
+
+    'p', 'k', 's', 'd', 'i', 'r', '0', 0
+};
+
 const u16 gPartners[10] = 
 {
     SPECIES_CHARMANDER,
