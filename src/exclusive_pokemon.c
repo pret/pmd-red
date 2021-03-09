42858bc89604855438c73ec186d019630659764d
Seth Barberee <seth.barberee@gmail.com>
2021-03-09 12:10:48 -0600

    split exclusive pokemon data out into C

--- src/exclusive_pokemon.c
+++ src/exclusive_pokemon.c
@@ -1,4 +1,5 @@
 #include "global.h"
+#include "constants/species.h"
 #define NUM_EXCLUSIVE_POKEMON 12
 
 struct ExclusivePokemon
@@ -7,7 +8,81 @@ struct ExclusivePokemon
     bool8 in_rrt; // red rescue team
     bool8 in_brt; // blue rescue team
 };
-extern struct ExclusivePokemon gExclusivePokemon[];
+
+const struct ExclusivePokemon gExclusivePokemon[NUM_EXCLUSIVE_POKEMON] = {
+
+    {
+        .poke_id = SPECIES_PORYGON,
+        .in_rrt = TRUE,
+        .in_brt = FALSE,
+    },
+    {
+        .poke_id = SPECIES_PORYGON2,
+        .in_rrt = FALSE,
+        .in_brt = TRUE,
+    },
+    {
+        .poke_id = SPECIES_MAGIKARP,
+        .in_rrt = FALSE,
+        .in_brt = TRUE,
+    },
+    {
+        .poke_id = SPECIES_GYARADOS,
+        .in_rrt = FALSE,
+        .in_brt = TRUE,
+    },
+    {
+        .poke_id = SPECIES_FEEBAS,
+        .in_rrt = TRUE,
+        .in_brt = FALSE,
+    },
+    {
+        .poke_id = SPECIES_MILOTIC,
+        .in_rrt = TRUE,
+        .in_brt = FALSE,
+    },
+    {
+        .poke_id = SPECIES_PLUSLE,
+        .in_rrt = TRUE,
+        .in_brt = FALSE,
+    },
+    {
+        .poke_id = SPECIES_MINUN,
+        .in_rrt = FALSE,
+        .in_brt = TRUE,
+    },
+    {
+        .poke_id = SPECIES_LAPRAS,
+        .in_rrt = FALSE,
+        .in_brt = TRUE,
+    },
+    {
+        .poke_id = SPECIES_MANTINE,
+        .in_rrt = TRUE,
+        .in_brt = FALSE,
+    },
+    {
+        .poke_id = SPECIES_ROSELIA,
+        .in_rrt = TRUE,
+        .in_brt = FALSE,
+    },
+    {
+        .poke_id = SPECIES_AIPOM,
+        .in_rrt = FALSE,
+        .in_brt = TRUE,
+    },
+
+};
+
+const u8 filler_ex0[8] = 
+{
+    'p', 'k', 's', 'd', 'i', 'r', '0', 0
+};
+const u8 filler_ex1[8] = 
+{
+    'p', 'k', 's', 'd', 'i', 'r', '0', 0
+};
+
 
 struct unkStruct_203B498
 {
