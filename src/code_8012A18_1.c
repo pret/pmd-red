aba4cb130b1fed6c1b0b18fd1c5d38abae3a0f93
Seth Barberee <seth.barberee@gmail.com>
2021-05-22 17:05:02 -0500

    decomp a little more

--- src/code_8012A18_1.c
+++ src/code_8012A18_1.c
@@ -1,12 +1,16 @@
 #include "global.h"
 #include "menu.h"
 #include "file_system.h"
+#include "memory.h"
+#include "input.h"
+
+extern void sub_800641C(u32, u32, u32);
 
 struct unkStruct_203B200
 {
-    u8 fill0[0x4];
+    u32 unk0;
     u32 unk4;
-    u8 *unk8;
+    u8 *pokeName;
     s32 state;
     /* 0x10 */ struct MenuItem menus[3];
 };
@@ -24,23 +28,95 @@ extern void sub_8014248(const char *r0, u32, u32, const struct MenuItem *r4, u32
 
 extern s32 sub_80144A4(s32 *);
 extern void sub_80151C0(u32, u8 *);
-extern void sub_80161C8(void);
+extern void CreateConfirmNameMenu(void);
 extern void sub_80160F4(u32);
-extern u8 sub_80162B8(void);
+bool8 sub_80162B8(void);
 extern u32 sub_80154F0(void);
-extern s32 sub_8015FEC(u8 *, u32);
-extern s32 sub_8016028(void);
 extern void sub_80155F0(void);
 extern void sub_80922B4(u8 *, u8 *, s32);
 
 void nullsub_36(void);
 void sub_8016110(void);
 extern void sub_80161F8(void);
-extern void sub_8016248(void);
+extern void HandleConfirmNameMenu(void);
 extern void sub_8016280(void);
 extern void sub_801629C(void);
 
 
+enum
+{
+    MENU_ACTION_DEFAULT = 1,
+    MENU_ACTION_YES,
+    MENU_ACTION_NO,
+};
+
+extern u32 ReturnIntFromChar2(u8);
+
+struct unkChar
+{
+    u32 unk0;
+    s16 unk4;
+    s16 unk6;
+};
+
+struct unkChar *GetCharacter(u32);
+
+
+s32 sub_8015FEC(u8 *buffer, s32 size)
+{
+  struct unkChar *iVar2;
+  s32 currLength;
+  s32 iVar4;
+  u8 *bufferPtr;
+  
+  iVar4 = 0;
+  currLength = 0;
+
+ if(currLength < size && *buffer != '\0')
+ {
+    bufferPtr = buffer; // Had to have another variable to match..
+    do {
+        iVar2 = GetCharacter(ReturnIntFromChar2(*bufferPtr));
+        iVar4 += iVar2->unk6;
+        bufferPtr++;
+        currLength++;
+    } while (currLength < size && *bufferPtr != '\0');
+ }
+  return iVar4;
+}
+
+s32 sub_8016028(void)
+{
+    return 60;
+}
+
+u32 sub_801602C(u32 r0, u8 *name)
+{
+    ResetUnusedInputStruct();
+    sub_800641C(0, 1, 1);
+    gUnknown_203B200 = MemoryAlloc(0x50, 8);
+    gUnknown_203B200->unk0 = r0;
+    gUnknown_203B200->pokeName = name;
+    switch(r0)
+    {
+        case 0:
+            gUnknown_203B200->unk4 = 0;
+            break;
+        case 1:
+            gUnknown_203B200->unk4 = 1;
+            break;
+        case 2:
+            gUnknown_203B200->unk4 = 2;
+            break;
+        case 3:
+            gUnknown_203B200->unk4 = 3;
+            break;
+        default:
+            break;
+    }
+    sub_80160F4(0);
+    return 1;
+}
 
 u32 sub_8016080(void)
 {
@@ -49,7 +125,7 @@ u32 sub_8016080(void)
         sub_80161F8();
         break;
     case 1:
-        sub_8016248();
+        HandleConfirmNameMenu();
         break;
     case 2:
         sub_8016280();
@@ -91,15 +167,15 @@ void sub_8016110(void)
   switch(gUnknown_203B200->state)
   {
     case 0:
-        sub_80151C0(gUnknown_203B200->unk4,gUnknown_203B200->unk8);
+        sub_80151C0(gUnknown_203B200->unk4,gUnknown_203B200->pokeName);
         break;
     case 1:
-        sub_80161C8();
+        CreateConfirmNameMenu();
         if (gUnknown_203B200->unk4 == 1) {
-            sub_80922B4(gAvailablePokemonNames,gUnknown_203B200->unk8,10);
+            sub_80922B4(gAvailablePokemonNames,gUnknown_203B200->pokeName,10);
         }
         else {
-            sub_80922B4(gAvailablePokemonNames,gUnknown_203B200->unk8,10);
+            sub_80922B4(gAvailablePokemonNames,gUnknown_203B200->pokeName,10);
         }
         // Is the name #C5$m0#R OK?
         sub_8014248(&gUnknown_80DB688,0,3,gUnknown_203B200->menus,0,4,0,0,0x20);
@@ -118,30 +194,30 @@ void sub_8016110(void)
   }
 }
 
-void sub_80161C8(void)
+void CreateConfirmNameMenu(void)
 { 
   gUnknown_203B200->menus[0].text = gUnknown_80D4920; // Yes
-  gUnknown_203B200->menus[0].menuAction = 2;
+  gUnknown_203B200->menus[0].menuAction = MENU_ACTION_YES;
   gUnknown_203B200->menus[1].text = gUnknown_80D4928; // No
-  gUnknown_203B200->menus[1].menuAction = 3;
+  gUnknown_203B200->menus[1].menuAction = MENU_ACTION_NO;
   gUnknown_203B200->menus[2].text = NULL;
-  gUnknown_203B200->menus[2].menuAction = 1;
+  gUnknown_203B200->menus[2].menuAction = MENU_ACTION_DEFAULT;
 }
 
 void sub_80161F8(void)
 {
   s32 iVar2;
-  s32 iVar3;
+  s32 maxLength;
   
   if (sub_80154F0() == 3) {
     sub_80155F0();
-    if (sub_80162B8() != 0) {
+    if (sub_80162B8()) {
         sub_80160F4(3);
     }
     else{
-      iVar2 = sub_8015FEC(gUnknown_203B200->unk8,10);
-      iVar3 = sub_8016028();
-      if (iVar2 > iVar3) {
+      iVar2 = sub_8015FEC(gUnknown_203B200->pokeName,10);
+      maxLength = sub_8016028(); // returns 60
+      if (iVar2 > maxLength) {
         sub_80160F4(2);
       }
       else {
@@ -151,18 +227,18 @@ void sub_80161F8(void)
   }
 }
 
-void sub_8016248(void)
+void HandleConfirmNameMenu(void)
 {
   s32 temp;
 
   if (sub_80144A4(&temp) == 0) {
     switch(temp)
     {
-        case 2:
+        case MENU_ACTION_YES:
             sub_80160F4(4);
             break;
-        case 3:
-        case 1:
+        case MENU_ACTION_NO:
+        case MENU_ACTION_DEFAULT:
             sub_80160F4(0);
             break;
         default:
@@ -190,18 +266,18 @@ void sub_801629C(void)
   }
 }
 
-u8 sub_80162B8(void)
+bool8 sub_80162B8(void)
 {
-  char cVar1;
-  int index;
+  u8 character;
+  s32 index;
   s32 max;
 
   max = 10; // constant needed at top of loop to match
   index = 0;
   do {
-    cVar1 = gUnknown_203B200->unk8[index];
-    if (cVar1 == 0) break;
-    if (cVar1 != 0x20) return 0;
+    character = gUnknown_203B200->pokeName[index];
+    if (character == '\0') break;
+    if (character != ' ') return 0;
     index++;
   } while (index < max);
   return 1;
