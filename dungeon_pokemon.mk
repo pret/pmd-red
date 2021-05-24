f4fa878ca33cac6e3abf809ec60ea29ebcf05287
Seth Barberee <seth.barberee@gmail.com>
2021-05-24 08:12:03 -0500

    convert sinister woods

--- dungeon_pokemon.mk
+++ dungeon_pokemon.mk
@@ -4,11 +4,14 @@ DUNGEONS_DIR = $(DATA_ASM_SUBDIR)/dungeon
 
 DUNGEON_DIRS := $(dir $(wildcard $(DUNGEONS_DIR)/*/pokemon_found.json))
 
-dungeon_pokemon: TinyWoods ThunderwaveCave
+dungeon_pokemon: TinyWoods ThunderwaveCave SinisterWoods
 
 TinyWoods: $(DUNGEONS_DIR)/TinyWoods/pokemon_found.json
 	$(DUNGEONJSON) dungeon pmd-red $(DUNGEONS_DIR)/TinyWoods/pokemon_found.json
 
 ThunderwaveCave: $(DUNGEONS_DIR)/ThunderwaveCave/pokemon_found.json
 	$(DUNGEONJSON) dungeon pmd-red $(DUNGEONS_DIR)/ThunderwaveCave/pokemon_found.json
+
+SinisterWoods: $(DUNGEONS_DIR)/SinisterWoods/pokemon_found.json
+	$(DUNGEONJSON) dungeon pmd-red $(DUNGEONS_DIR)/SinisterWoods/pokemon_found.json
 
