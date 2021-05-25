81761e38e0bc87dc663ac7e1d453220b23bacdce
Seth Barberee <seth.barberee@gmail.com>
2021-05-25 15:26:56 -0500

    convert silent chasm

--- dungeon_pokemon.mk
+++ dungeon_pokemon.mk
@@ -4,7 +4,7 @@ DUNGEONS_DIR = $(DATA_ASM_SUBDIR)/dungeon
 
 DUNGEON_DIRS := $(dir $(wildcard $(DUNGEONS_DIR)/*/pokemon_found.json))
 
-dungeon_pokemon: TinyWoods ThunderwaveCave SinisterWoods MtThunderPeak
+dungeon_pokemon: TinyWoods ThunderwaveCave SinisterWoods SilentChasm MtThunderPeak
 
 TinyWoods: $(DUNGEONS_DIR)/TinyWoods/pokemon_found.json
 	$(DUNGEONJSON) dungeon pmd-red $(DUNGEONS_DIR)/TinyWoods/pokemon_found.json
@@ -14,6 +14,9 @@ ThunderwaveCave: $(DUNGEONS_DIR)/ThunderwaveCave/pokemon_found.json
 
 SinisterWoods: $(DUNGEONS_DIR)/SinisterWoods/pokemon_found.json
 	$(DUNGEONJSON) dungeon pmd-red $(DUNGEONS_DIR)/SinisterWoods/pokemon_found.json
+
+SilentChasm: $(DUNGEONS_DIR)/SilentChasm/pokemon_found.json
+	$(DUNGEONJSON) dungeon pmd-red $(DUNGEONS_DIR)/SilentChasm/pokemon_found.json
 
 MtThunderPeak: $(DUNGEONS_DIR)/MtThunderPeak/pokemon_found.json
 	$(DUNGEONJSON) dungeon pmd-red $(DUNGEONS_DIR)/MtThunderPeak/pokemon_found.json
