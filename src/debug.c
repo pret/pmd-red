8f72be34b230b87a3afd10f3fb994c218b109053
Seth Barberee <seth.barberee@gmail.com>
2021-07-27 22:24:52 -0700

    no need to preproc pure ascii strings in debug

--- src/debug.c
+++ src/debug.c
@@ -10,50 +10,47 @@ static void FatalErrorHang(void) __attribute__((noreturn));
 extern bool32 gNDS_DebugEnabled;
 extern u8 gUnknown_203B150;
 
-ALIGNED(4) const char gFuncFileLineString[] = _("func = '%s'\n"
-                                                "file = '%s'  line = %5d");
+ALIGNED(4) const char gFuncFileLineString[] = "func = '%s'\nfile = '%s'  line = %5d";
 
-ALIGNED(4) const char gNotEntryText[] = _("--- not entry ---");
-ALIGNED(4) const char gFuncFileLineStringWPrefix[] = _("%sfunc = '%s'\n"
-                                                       "file = '%s'  line = %5d\n");
+ALIGNED(4) const char gNotEntryText[] = "--- not entry ---";
+ALIGNED(4) const char gFuncFileLineStringWPrefix[] = "%sfunc = '%s'\nfile = '%s'  line = %5d\n";
 
-ALIGNED(4) const char gFuncFileLineString2[] = _("func = '%s'\n"
-                                                 "file = '%s'  line = %5d\n");
+ALIGNED(4) const char gFuncFileLineString2[] = "func = '%s'\nfile = '%s'  line = %5d\n";
 
-ALIGNED(4) const char debug_fill14[] = _("pksdir0");
-ALIGNED(4) const char gDebugPrintPrefix[] = _("  Print  ");
-ALIGNED(4) const char debug_fill13[] = _("pksdir0");
+ALIGNED(4) const char debug_fill14[] = "pksdir0";
+ALIGNED(4) const char gDebugPrintPrefix[] = "  Print  ";
+ALIGNED(4) const char debug_fill13[] = "pksdir0";
 
-ALIGNED(4) const char Performance_Text[] = _("Performance");
-ALIGNED(4) const char MemoryCard_Text[] = _("Memory Card");
-ALIGNED(4) const char Memory_Text[] = _("Memory");
-ALIGNED(4) const char Flag_Text[] = _("Flag");
-ALIGNED(4) const char Se_Text[] = _("Se");
-ALIGNED(4) const char Bgm_Text[] = _("Bgm");
-ALIGNED(4) const char Sound_Text[] = _("Sound");
-ALIGNED(4) const char Dungeon_Text[] = _("Dungeon");
-ALIGNED(4) const char GroundScript_Text[] = _("GroundScript");
-ALIGNED(4) const char Ground_Text[] = _("Ground");
+ALIGNED(4) const char Performance_Text[] = "Performance";
+ALIGNED(4) const char MemoryCard_Text[] = "Memory Card";
+ALIGNED(4) const char Memory_Text[] = "Memory";
+ALIGNED(4) const char Flag_Text[] = "Flag";
+ALIGNED(4) const char Se_Text[] = "Se";
+ALIGNED(4) const char Bgm_Text[] = "Bgm";
+ALIGNED(4) const char Sound_Text[] = "Sound";
+ALIGNED(4) const char Dungeon_Text[] = "Dungeon";
+ALIGNED(4) const char GroundScript_Text[] = "GroundScript";
+ALIGNED(4) const char Ground_Text[] = "Ground";
 
 
-ALIGNED(4) const char gNotMountText[] = _("not mount log system");
-ALIGNED(4) const char debug_fill9[] = _("pksdir0");
-ALIGNED(4) const char debug_fill10[] = _("pksdir0");
-ALIGNED(4) const char debug_fill11[] = _("pksdir0");
-ALIGNED(4) const char debug_fill12[] = _("pksdir0");
+ALIGNED(4) const char gNotMountText[] = "not mount log system";
+ALIGNED(4) const char debug_fill9[] = "pksdir0";
+ALIGNED(4) const char debug_fill10[] = "pksdir0";
+ALIGNED(4) const char debug_fill11[] = "pksdir0";
+ALIGNED(4) const char debug_fill12[] = "pksdir0";
 
-ALIGNED(4) const char gFatalText[] = _("!!!!! Fatal !!!!!\n");
-ALIGNED(4) const char gFatalErrorBufferPlaceholder[] = _("%s\n");
+ALIGNED(4) const char gFatalText[] = "!!!!! Fatal !!!!!\n";
+ALIGNED(4) const char gFatalErrorBufferPlaceholder[] = "%s\n";
 
-ALIGNED(4) const char debug_fill0[] = _("pksdir0");
-ALIGNED(4) const char debug_fill1[] = _("pksdir0");
-ALIGNED(4) const char debug_fill2[] = _("pksdir0");
-ALIGNED(4) const char debug_fill3[] = _("pksdir0");
-ALIGNED(4) const char debug_fill4[] = _("pksdir0");
-ALIGNED(4) const char debug_fill5[] = _("pksdir0");
-ALIGNED(4) const char debug_fill6[] = _("pksdir0");
-ALIGNED(4) const char debug_fill7[] = _("pksdir0");
-ALIGNED(4) const char debug_fill8[] = _("pksdir0");
+ALIGNED(4) const char debug_fill0[] = "pksdir0";
+ALIGNED(4) const char debug_fill1[] = "pksdir0";
+ALIGNED(4) const char debug_fill2[] = "pksdir0";
+ALIGNED(4) const char debug_fill3[] = "pksdir0";
+ALIGNED(4) const char debug_fill4[] = "pksdir0";
+ALIGNED(4) const char debug_fill5[] = "pksdir0";
+ALIGNED(4) const char debug_fill6[] = "pksdir0";
+ALIGNED(4) const char debug_fill7[] = "pksdir0";
+ALIGNED(4) const char debug_fill8[] = "pksdir0";
 
 void NDS_DebugInit(void)
 {
