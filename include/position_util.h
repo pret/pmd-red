52e7368f30bb1a80a470bb1492b816dd6c8e97f7
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2022-01-08 00:08:00 -0600

    Labeled CalculateFacingDir()

---
+++ include/position_util.h
@@ -0,0 +1,9 @@
+#ifndef GUARD_POSITION_UTIL_H
+#define GUARD_POSITION_UTIL_H
+
+#include "position.h"
+
+// 0x983D8
+s32 CalculateFacingDir(struct Position *originPos, struct Position *targetPos);
+
+#endif
