84107163ef01781a780ee2f77dcf5f30d6e02514
Seth Barberee <seth.barberee@gmail.com>
2022-05-12 19:52:07 -0700

    move some more data to src

--- data/data_8106FDD.s
+++
@@ -1,6 +0,0 @@
-        .section .rodata
-
-	.global gDirectionBitMasks_3
-gDirectionBitMasks_3: @ 8106FDD
-@ replacing .incbin "baserom.gba", 0x00106fdd, 0x8
-        .byte 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80
