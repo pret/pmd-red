ecb8582e872df31d61650eb838ed54de8079eb89
Seth Barberee <seth.barberee@gmail.com>
2022-05-14 18:40:58 -0700

    dump and fix some more pointers

--- data/data_8107010.s
+++ data/data_8107010.s
@@ -2,9 +2,10 @@
 
         .string "pksdir0\0"
         .string "pksdir0\0"
+
 	.global gUnknown_8107010
 gUnknown_8107010: @ 8107010
-	.incbin "baserom.gba", 0x107010, 0x8
+        .byte 0, 1, 1, 2, 1, 1, 0, 0
 
 	.global gUnknown_8107018
 gUnknown_8107018: @ 8107018
