11af1a98cbe5e4bec32795eb031c2eeaef168438
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2022-10-17 20:49:41 -0400

    Changed do/while to for loop

--- src/dungeon_leader.c
+++ src/dungeon_leader.c
@@ -11,8 +11,8 @@ struct DungeonEntity* GetLeaderEntity()
     struct DungeonEntity *leader = gLeaderPokemon;
     if (leader == NULL)
     {
-        s32 i = 0;
-        do
+        s32 i;
+        for (i = 0; i < MAX_TEAM_MEMBERS; i++)
         {
             struct DungeonEntity *currentPokemon = gDungeonGlobalData->teamPokemon[i];
             if (EntityExists(currentPokemon) && currentPokemon->entityData->isLeader)
@@ -20,7 +20,7 @@ struct DungeonEntity* GetLeaderEntity()
                 gLeaderPokemon = currentPokemon;
                 return currentPokemon;
             }
-        } while (++i < MAX_TEAM_MEMBERS);
+        }
         return NULL;
     }
     return leader;
