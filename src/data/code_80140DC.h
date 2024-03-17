f5bd176a12d88d26eb6b97d23c5dcb67373ed42d
Seth Barberee <seth.barberee@gmail.com>
2024-03-17 14:15:20 -0700

    continue moving data

---
+++ src/data/code_80140DC.h
@@ -0,0 +1,25 @@
+const u8 Yes_80D4878[];
+const u8 No_80D4874[];
+
+const MenuItem gUnknown_80D485C[] = 
+{
+        {Yes_80D4878, 1},
+        {No_80D4874, 0},
+        {NULL, -1}
+};
+
+ALIGNED(4) const u8 No_80D4874[] = "No";
+ALIGNED(4) const u8 Yes_80D4878[] = "*Yes";
+
+const u8 Yes_80D489C[];
+const u8 No_80D4898[];
+
+const MenuItem gUnknown_80D4880[] = 
+{
+        {Yes_80D489C, 1},
+        {No_80D4898, 0},
+        {NULL, -1}
+};
+
+ALIGNED(4) const u8 No_80D4898[] = "*No";
+ALIGNED(4) const u8 Yes_80D489C[] = "Yes";
