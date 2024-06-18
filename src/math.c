09dc26aca0f60797a24e50265b30f00b48db6a15
Seth Barberee <seth.barberee@gmail.com>
2024-06-18 09:57:32 -0700

    decomp more math funcs

--- src/math.c
+++ src/math.c
@@ -2,9 +2,9 @@
 #include "globaldata.h"
 #include "math.h"
 
-extern u24_8 u24_8_div(u24_8 x, u24_8 y); // decomp in progress: https://decomp.me/scratch/pMyGD
-
 #include "data/math.h"
+
+u24_8 u24_8_div(u24_8 x, u24_8 y);
 
 u32 fast_mod_3(s32 x) {
     if (x < 0x100) {
@@ -158,4 +158,166 @@ u24_8 u24_8_mul(u24_8 x, u24_8 y) {
     }
 
     return out_l;
+}
+
+u24_8 u24_8_div(u24_8 x, u24_8 y)
+{
+  bool8 bVar1;
+  u32 r9;
+  u32 r2;
+  u32 r4;
+  u32 r5;
+  u32 r6;
+  u32 r7;
+  u32 r8;
+  int counter;
+  s32 sl;
+  s32 flag;
+  s32 save;
+  
+  if (y == 0) {
+    return 0x7fffffff;
+  }
+  else if (x == 0) {
+    return 0;
+  }
+  else {
+    r7 = x >> 0x18;
+    r6 = x << 8;
+    sl = y;
+    r9 = 0;
+    r5 = 0;
+    r4 = 0;
+    r2 = 0;
+    r8 = 1;
+
+      
+    for(counter = 0x3f; counter >= 0; counter--)
+    {
+      r5 <<= 1;
+      if ((r4 & 0x80000000) != 0) {
+        r5 |= r8;
+      }
+      flag = -2;
+      r4 <<= 1;
+      if ((r7 & 0x80000000) != 0) {
+        r4 |= r8;
+      }
+      r7 <<= 1;
+      if ((r6 & 0x80000000) != 0) {
+        r7 |= r8;
+      }
+      r6 <<= 1;
+      r6 &= flag;
+      if (u32_pair_less_than(r5,r4,0,sl) == 0) {
+        save = r4;
+        bVar1 = 1;
+        r4 = r4 - sl;
+        r5 = r5 - r2;  // WHY????
+        if (save < r4) {
+          r5--;
+        }
+      }
+      else {
+        bVar1 = 0;
+      }
+      r9 <<= 1;
+      if (bVar1) {
+        r9 |= r8;
+      }
+    }
+  }
+  return r9;
+}
+
+s32 sub_8009F68(s32 x, s32 y)
+{
+    s32 uVar1;
+    s32 sVar1;
+
+    uVar1 = y;
+    if (y < 0) {
+        uVar1 = -y;
+    }
+    sVar1 = 0x100;
+    for (; uVar1 != 0; uVar1 >>= 1) {
+        if ((uVar1 & 1) != 0) {
+            sVar1 = s24_8_mul(sVar1, x);
+        }
+        x = s24_8_mul(x, x);
+    }
+    if (y >= 0) {
+        return sVar1;
+    }
+    else
+        return s24_8_div(0x100,sVar1);
+}
+
+s32 sub_8009FB8(s32 x, s32 y)
+{
+    s32 r4;
+    s32 counter;
+
+    s32 r5;
+    s32 r6;
+
+    r5 = x;
+    r6 = y;
+
+    if (r5 < 0) {
+        r5 = -r5;
+    }
+    if (r6 < 0) {
+        r6 = -r6;
+    }
+    if (r5 < r6) {
+        r4 = r5;
+        r5 = r6;
+        r6 = r4;
+    }
+    if (r6 != 0) {
+        for(counter = 2; counter >= 0; counter--)
+        {
+            r4 = s24_8_div(r6,r5);
+            r4 = s24_8_mul(r4,r4);
+            r4 = s24_8_div(r4,r4 + 0x400);
+            r5 = r5 + s24_8_mul(r5,r4) * 2;
+            r6 = s24_8_mul(r6,r4);
+        }
+    }
+    return r5;
+}
+
+void sub_800A020(s32 *param_1, u32 param_2)
+{
+  register u32 temp asm("r4");
+    
+  temp = 0xffff0000;
+  param_1[0] = param_2 >> 0x10;
+  param_1[1] = param_2 << 0x10;
+  if ((param_2 & 0x8000) != 0) {
+    param_1[0] |= temp;
+  }
+}
+
+u32 sub_800A048(u32 *param_1)
+{
+  u32 uVar1;
+  
+  uVar1 = ((u16)param_1[0] << 0x10) | (param_1[1] >> 0x10);
+  if ((param_1[1] & 0x8000) != 0) {
+    uVar1++;
+  }
+  return uVar1;
+}
+
+UNUSED u32 sub_800A068(u32 *param_1)
+{
+  u32 uVar1;
+  
+  uVar1 = ((u8)param_1[0] << 0x18) | param_1[1] >> 8;
+  if ((param_1[1] & 0x8000) != 0) {
+    uVar1++;
+  }
+  return uVar1;
 }
