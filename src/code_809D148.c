2de2dc0cb95994aea84edc0084ffc53672c567a2
DizzyEggg <jajkodizzy@wp.pl>
2024-07-17 16:44:25 +0200

    decompile more of code_809D148

--- src/code_809D148.c
+++ src/code_809D148.c
@@ -1,5 +1,7 @@
 #include "global.h"
 #include "other_random.h"
+#include "memory.h"
+#include "ground_script.h"
 
 struct unkStruct_809D158
 {
@@ -16,8 +18,8 @@ struct unkStruct_20399E8
     s32 unk10;
     struct unkStruct_809D158 unk14;
     struct unkStruct_809D158 unk1C;
-    u32 unk24;
-    u32 unk28;
+    s32 unk24;
+    s32 unk28;
     s32 unk2C;
     s32 unk30;
     s32 unk34;
@@ -29,7 +31,8 @@ struct unkStruct_20399E8
 extern struct unkStruct_20399E8 gUnknown_20399E8;
 
 void sub_809D0BC(void);
-void sub_809D158(u32 r0, const struct unkStruct_809D158 *r1);
+void sub_809D4B0(void);
+void sub_809D158(s32 r0, const struct unkStruct_809D158 *r1);
 
 s32 sub_809CFE8(u16 param_1)
 {
@@ -121,7 +124,7 @@ void sub_809D0BC(void)
     gUnknown_20399E8.unk38 = 0;
 }
 
-void sub_809D100(u32 r0)
+void sub_809D100(s32 r0)
 {
     if(gUnknown_20399E8.unk24 != r0)
     {
@@ -134,7 +137,7 @@ void sub_809D100(u32 r0)
     }
 }
 
-void sub_809D124(u32 r0, u32 r1, u32 r2)
+void sub_809D124(s32 r0, s32 r1, s32 r2)
 {
     if(gUnknown_20399E8.unk24 != r0)
     {
@@ -147,7 +150,7 @@ void sub_809D124(u32 r0, u32 r1, u32 r2)
     gUnknown_20399E8.unk2C = r2;
 }
 
-void sub_809D148(u32 r0)
+void sub_809D148(s32 r0)
 {
     gUnknown_20399E8.unk0 = r0;
     gUnknown_20399E8.unk8 = 0;
@@ -159,28 +162,28 @@ struct Test1
     u64 ukn0;
 };
 
-void sub_809D158(u32 r0, const struct unkStruct_809D158 *r1)
+void sub_809D158(s32 r0, const struct unkStruct_809D158 *r1)
 {
     gUnknown_20399E8.unk0 = r0;
     gUnknown_20399E8.unk8 = 0;
     gUnknown_20399E8.unk1C = *r1;
 }
 
-void sub_809D170(u32 r0, u32 r1)
+void sub_809D170(s32 r0, s32 r1)
 {
     gUnknown_20399E8.unk0 = r0;
     gUnknown_20399E8.unk8 = 0;
     gUnknown_20399E8.unk4 = r1;
 }
 
-void sub_809D180(u32 r0, u32 r1)
+void sub_809D180(s32 r0, s32 r1)
 {
     gUnknown_20399E8.unk0 = r0;
     gUnknown_20399E8.unk8 = 1;
     gUnknown_20399E8.unkC = r1;
 }
 
-void sub_809D190(u32 r0, struct unkStruct_809D158 *r1, u32 r2)
+void sub_809D190(s32 r0, struct unkStruct_809D158 *r1, s32 r2)
 {
     gUnknown_20399E8.unk0 = r0;
     gUnknown_20399E8.unk8 = 1;
@@ -188,7 +191,7 @@ void sub_809D190(u32 r0, struct unkStruct_809D158 *r1, u32 r2)
     gUnknown_20399E8.unk14 = *r1;
 }
 
-void sub_809D1A8(u32 r0, u32 r1, u32 r2)
+void sub_809D1A8(s32 r0, s32 r1, s32 r2)
 {
     gUnknown_20399E8.unk0 = r0;
     gUnknown_20399E8.unk8 = 1;
@@ -196,14 +199,14 @@ void sub_809D1A8(u32 r0, u32 r1, u32 r2)
     gUnknown_20399E8.unk4 = r1;
 }
 
-void sub_809D1BC(u32 r0, u32 r1)
+void sub_809D1BC(s32 r0, s32 r1)
 {
     gUnknown_20399E8.unk0 = r0;
     gUnknown_20399E8.unk8 = 2;
     gUnknown_20399E8.unk10 = r1;
 }
 
-void sub_809D1CC(u32 r0, struct unkStruct_809D158 *r1, u32 r2)
+void sub_809D1CC(s32 r0, struct unkStruct_809D158 *r1, s32 r2)
 {
     gUnknown_20399E8.unk0 = r0;
     gUnknown_20399E8.unk8 = 2;
@@ -211,7 +214,7 @@ void sub_809D1CC(u32 r0, struct unkStruct_809D158 *r1, u32 r2)
     gUnknown_20399E8.unk14 = *r1;
 }
 
-void sub_809D1E4(u32 r0, u32 r1, u32 r2)
+void sub_809D1E4(s32 r0, s32 r1, s32 r2)
 {
     gUnknown_20399E8.unk0 = r0;
     gUnknown_20399E8.unk8 = 2;
@@ -219,14 +222,14 @@ void sub_809D1E4(u32 r0, u32 r1, u32 r2)
     gUnknown_20399E8.unk4 = r1;
 }
 
-void sub_809D1F8(u32 r0, u32 r1)
+void sub_809D1F8(s32 r0, s32 r1)
 {
     gUnknown_20399E8.unk0 = r0;
     gUnknown_20399E8.unk8 = 3;
     gUnknown_20399E8.unkC = r1;
 }
 
-void sub_809D208(u32 r0, struct unkStruct_809D158 *r1, u32 r2)
+void sub_809D208(s32 r0, struct unkStruct_809D158 *r1, s32 r2)
 {
     gUnknown_20399E8.unk0 = r0;
     gUnknown_20399E8.unk8 = 3;
@@ -234,7 +237,7 @@ void sub_809D208(u32 r0, struct unkStruct_809D158 *r1, u32 r2)
     gUnknown_20399E8.unk14 = *r1;
 }
 
-void sub_809D220(u32 r0, u32 r1, u32 r2)
+void sub_809D220(s32 r0, s32 r1, s32 r2)
 {
     gUnknown_20399E8.unk0 = r0;
     gUnknown_20399E8.unk8 = 3;
@@ -253,13 +256,14 @@ u8 sub_809D248(struct unkStruct_809D158 *r0)
     return gUnknown_20399E8.unk44;
 }
 
-s32 sub_80A8FD8(s16 a0, struct unkStruct_809D158 *a1);
-s32 sub_80AC448(s16 a0, struct unkStruct_809D158 *a1);
-s32 sub_80AD360(s16 a0, struct unkStruct_809D158 *a1);
-void sub_800290C(struct unkStruct_809D158 *a0, s32 a1);
-void sub_8002934(struct unkStruct_809D158 *a0, struct unkStruct_809D158 *a1, struct unkStruct_809D158 *a2, u32 a3, u32 a4);
-s32 sub_8009FB8(s32 a0, s32 a1);
-bool8 sub_80A579C(struct unkStruct_809D158 *a0, struct unkStruct_809D158 *a1);
+// Todo: move these to appropriate header files
+extern s32 sub_80A8FD8(s16 a0, struct unkStruct_809D158 *a1);
+extern s32 sub_80AC448(s16 a0, struct unkStruct_809D158 *a1);
+extern s32 sub_80AD360(s16 a0, struct unkStruct_809D158 *a1);
+extern void sub_800290C(struct unkStruct_809D158 *a0, s32 a1);
+extern void sub_8002934(struct unkStruct_809D158 *a0, struct unkStruct_809D158 *a1, struct unkStruct_809D158 *a2, u32 a3, u32 a4);
+extern s32 sub_8009FB8(s32 a0, s32 a1);
+extern bool8 sub_80A579C(struct unkStruct_809D158 *a0, struct unkStruct_809D158 *a1);
 
 void sub_809D25C(void)
 {
@@ -367,4 +371,115 @@ void sub_809D25C(void)
         gUnknown_20399E8.unk3C.a4 = (r3 - 20480) / 256;
         gUnknown_20399E8.unk44 = 1;
     }
+}
+
+void nullsub_120(void)
+{}
+
+// Different file below?
+
+extern void *gUnknown_203B4B0;
+
+void sub_809D490(void)
+{
+    UNUSED void *oldPtr = gUnknown_203B4B0; // Needed to match
+    gUnknown_203B4B0 = MemoryAlloc(0x400, 6);
+    sub_809D4B0();
+}
+
+extern s16 gUnknown_2039A30;
+extern s16 gUnknown_2039A32;
+extern s16 gUnknown_2039A34;
+extern u8 gUnknown_2039A36;
+extern u8 gUnknown_2039A38[];
+extern u8 gUnknown_2039AC0[];
+extern u32 gUnknown_2039B48[];
+
+void sub_809D4B0(void)
+{
+    s32 i;
+
+    gUnknown_2039A30 = -1;
+    gUnknown_2039A32 = -1;
+    gUnknown_2039A34 = -1;
+    gUnknown_2039A36 = 0;
+    for (i = 0; i < 0x81; i++) {
+        gUnknown_2039A38[i] = 0;
+        gUnknown_2039AC0[i] = 0;
+        gUnknown_2039B48[i] = 0;
+    }
+}
+
+void sub_809D508(void)
+{
+    MemoryFree(gUnknown_203B4B0);
+    gUnknown_203B4B0 = NULL;
+}
+
+// -1 didn't match
+void sub_809D520(void *a0)
+{
+    u16 *ptr = a0;
+    u16 v = 0xFFFF;
+    *ptr = v;
+}
+
+extern s32 sub_80A882C(s16);
+extern s32 sub_80AC240(s16);
+extern s32 sub_80AD158(s16);
+
+s32 sub_809D52C(void *a0)
+{
+    s16 *ptr = a0;
+
+    switch (ptr[0])
+    {
+    case 0: return 0;
+    case 1: return sub_80A882C(ptr[1]);
+    case 2: return sub_80AC240(ptr[1]);
+    case 3: return sub_80AD158(ptr[1]);
+    }
+    return 0;
+}
+
+void sub_809D568(GroundScript_ExecutePP_1_sub_not_packed *a0)
+{
+    s32 i;
+
+    a0->scriptType = -1;
+    a0->unk2 = 0;
+    a0->scriptPointer2 = 0;
+    a0->scriptPointer1 = 0;
+    a0->unk14 = 0;
+    a0->unk10 = 0;
+    a0->unk20 = 0;
+    a0->unk1C = 0;
+    a0->unk22 = -1;
+    a0->unk24 = 0;
+    a0->unk26 = 0xFF;
+    a0->unk28 = 0;
+    a0->unk2A = 0;
+    a0->unk2C = 0;
+    a0->unk30 = 0;
+    for (i = 0; i < 4; i++) {
+        a0->unk5C[i] = 0;
+    }
+}
+
+void sub_809D5B8(GroundScript_ExecutePP_1 *a0)
+{
+    s32 i;
+
+    a0->unk0 = 0;
+    a0->unk4 = 0;
+    a0->unk10 = -1;
+    a0->unk12 = 0xFF;
+    sub_809D520(&a0->unkC);
+
+    for (i = 0; i < 4; i++) {
+        a0->unk14[i] = 0;
+    }
+
+    sub_809D568((void *) &a0->unk24); // Casting to void* because packed/not packed
+    sub_809D568((void *) &a0->unk84); // Casting to void* because packed/not packed
 }
