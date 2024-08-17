d20d2a720a03ca9a7151496303eb453e53e2654f
Seth Barberee <seth.barberee@gmail.com>
2024-08-17 12:08:42 -0700

    decomp a few more funcs

--- src/code_800ED38.c
+++ src/code_800ED38.c
@@ -1,4 +1,5 @@
 #include "global.h"
+#include "file_system.h"
 #include "memory.h"
 
 struct unkStruct_203B0D0_sub
@@ -24,6 +25,16 @@ struct unkStruct_203B0D0 {
 struct unkStruct_203B0D0 *gUnknown_203B0D0;
 
 extern s32 sub_8000728();
+s32 *sub_800ECA4(s32);
+OpenedFile *sub_800F1C0(s32, s32);
+s32 sub_800F0F4(s32, s32);
+void sub_800F204(OpenedFile *file);
+void sub_800F13C(s32, OpenedFile *, s32 *);
+void sub_800F15C(s32);
+s16 *sub_800ECB8(u16);
+s16 sub_800ECE4(u8);
+s16 sub_800ECF8(u8 r0);
+s16 sub_800ED0C(u8 r0);
 
 void sub_800ED38(s32 r0)
 {
@@ -105,4 +116,86 @@ void sub_800EDF0(u32 r0, struct unkStruct_203B0D0_sub *r1) {
     stack.unk8 = r1->unk4;
     stack.unk20 = 0;
     sub_800EDB0(&stack);
+}
+
+void sub_800EE5C(s32 r0)
+{
+    s32 *ret;
+    OpenedFile *file;
+    s32 ret2;
+
+    ret = sub_800ECA4(r0);
+    if(r0 != 0)
+    {
+        if((u32)(ret[0] - 1) > 1)
+        {
+            file = sub_800F1C0(ret[0], ret[1]);
+            if(file)
+            {
+                ret2 = sub_800F0F4(ret[0], ret[1]);
+                if(ret2 == -1)
+                {
+                    sub_800F204(file);
+                    return;
+                }
+                else if(ret2 != -2)
+                {
+                    sub_800F13C(ret2, file, ret);
+                    sub_800EDF0(ret2, (void *)file);
+                    
+                }
+                else {
+                    sub_800F204(file);
+                }
+                sub_800F15C(ret[1]);
+            }
+        }
+    }
+}
+
+void sub_800EEC8(u16 r0)
+{
+    s16 *ret;
+    ret = sub_800ECB8(r0);
+    sub_800EE5C(ret[0]);
+}
+
+void sub_800EEE0(u16 r0)
+{
+    s16 *ret;
+    ret = sub_800ECB8(r0);
+    sub_800EE5C(ret[2]);
+}
+
+void sub_800EEF8(u16 r0)
+{
+    s16 *ret;
+    ret = sub_800ECB8(r0);
+    sub_800EE5C(ret[1]);
+}
+
+void sub_800EF10(u16 r0)
+{
+    s16 *ret;
+    ret = sub_800ECB8(r0);
+    sub_800EE5C(ret[3]);
+}
+
+void sub_800EF28(u8 r0)
+{
+    sub_800EE5C(sub_800ECE4(r0));
+}
+
+void sub_800EF40(u8 r0, u8 r1)
+{
+    s32 ret;
+    if(r1)
+    {
+        ret = sub_800ECF8(r0);
+    }
+    else
+    {
+        ret = sub_800ED0C(r0);
+    }
+    sub_800EE5C(ret);
 }
