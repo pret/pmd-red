cd2c46d027e31a2126ebe89c305a68b0d92b53f9
Seth Barberee <seth.barberee@gmail.com>
2024-12-15 10:08:16 -0800

    finish off pokemon_3.s thanks to @DizzyEggg

--- include/pokemon_3.h
+++ include/pokemon_3.h
@@ -15,6 +15,40 @@ typedef struct EvolveStatus
     /* 0x6 */ s16 targetEvolveSpecies;
 } EvolveStatus;
 
+struct unkStruct_808FF20
+{
+    // size: 0x64
+    s16 species;
+    u8 nameBuffer[0x14];
+    u8 types[2]; // 0x16
+    u8 abilities[2]; // 0x18
+    u8 fill1A[0x1C - 0x1A];
+     DungeonLocation dungeonLocation; // 0x1C
+    Item item; // 0x20
+    s32 HP1;
+    s32 HP2;
+    s32 level;
+    u32 exp;
+     Offense offense;
+    bool8 isTeamLeader;
+    u8 atkBoost;
+    u8 spAtkBoost;
+    u8 defBoost;
+    u8 spDefBoost;
+    u8 fill3D;
+    s16 IQ; // 0x3E
+    s16 unk40;
+    s16 fill42;
+    struct unkPokeSubStruct_C unk44[2];
+    u8 unk4C;
+    u8 fill4D[3];
+    IqSkillFlags IQSkills; // 0x50
+    u8 tactic;
+    u8 fill55[1];
+    s16 unk56;
+    u8 unk58[12];
+};
+
 bool8 HasRecruitedMon(s16 species);
 s16 GetBaseSpecies(s16 index);
 s16 GetBaseSpeciesNoUnown(s16 index);
@@ -36,9 +70,7 @@ s16 ExtractSpeciesIndex(UnkDungeonGlobal_unk1CD98 *r0);
 void SetSpeciesLevelToExtract(UnkDungeonGlobal_unk1CD98 *r0, s32 level, s32 species);
 s32 ExtractLevel(UnkDungeonGlobal_unk1CD98 *r0);
 PokemonStruct1 *sub_808F798(PokemonStruct1 *pokemon, short _species);
-
-// pokemon_3.s
-extern void CreatePokemonInfoTabScreen(u32, s16, u32 *, u32 *, u32);
-extern void sub_808FF20(u32 *, PokemonStruct1 *, bool8);
+void CreatePokemonInfoTabScreen(u32, s32, struct unkStruct_808FF20 *, u8 *, u32);
+void sub_808FF20(struct unkStruct_808FF20 *param_1, struct PokemonStruct1 *pokemon, bool8 param_3);
 
 #endif // GUARD_POKEMON_3_H
