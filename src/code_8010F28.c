8c21c7935a2e930468a5d20b7c7977400c88090c
DizzyEggg <jajkodizzy@wp.pl>
2025-02-21 14:25:35 +0100

    sub_8010A88

--- src/code_8010F28.c
+++ src/code_8010F28.c
@@ -14,35 +14,62 @@
 #include "play_time.h"
 #include "code_80118A4.h"
 #include "code_800C9CC.h"
+#include "pokemon.h"
+#include "decompress.h"
 
+extern void PlayAcceptSoundEffect(void);
+extern void PlayCursorUpDownSoundEffect(void);
+extern s32 sprintf(char *, const char *, ...);
 extern void sub_800CB20(void);
 extern void xxx_call_update_bg_sound_input(void);
 extern bool8 sub_800FFE8(u8);
 extern void sub_80101F8(u8 *, u8);
+extern void CopyDungeonName1toBuffer(u8 *buffer, DungeonLocation *dungeonLocation);
+
+struct UnkStruct_Sub1
+{
+    u8 unk0;
+    DungeonPos unk4;
+    axdata unk8;
+};
+
+struct UnkStruct_203B0E8
+{
+    struct UnkStruct_Sub1 unk0[64];
+    u8 pad1[0x4014];
+    axdata unk5114;
+    u8 pad2[0x184];
+    s32 unk52D4;
+    DungeonPos unk52D8;
+    s16 unk52DC;
+    s16 unk52DE;
+};
 
 EWRAM_INIT struct UnkBgStruct *gUnknown_203B0E4 = NULL;
-EWRAM_INIT void *gUnknown_203B0E8 = NULL; // Pointer to some unknown struct
-EWRAM_INIT UnkTextStruct2 gUnknown_203B0EC[4] = {
-    [0] = {
-        .unk4 = 3,
-        .pos = {2, 10},
-        .unkC = 26,
-        .unkE = 9,
-        .unk10 = 9,
-    },
-    [1] = {
-        .unk4 = 3,
-        .pos = {23, 6},
-        .unkC = 5,
-        .unkE = 3,
-        .unk10 = 3,
-    },
-    [2] = {
-        .unk4 = 3,
-    },
-    [3] = {
-        .unk4 = 3,
-    },
+EWRAM_INIT struct UnkStruct_203B0E8 *gUnknown_203B0E8 = NULL;
+EWRAM_INIT UnkTextStruct3 gUnknown_203B0EC = {
+    .a0 = {
+        [0] = {
+            .unk4 = 3,
+            .pos = {2, 10},
+            .unkC = 26,
+            .unkE = 9,
+            .unk10 = 9,
+        },
+        [1] = {
+            .unk4 = 3,
+            .pos = {23, 6},
+            .unkC = 5,
+            .unkE = 3,
+            .unk10 = 3,
+        },
+        [2] = {
+            .unk4 = 3,
+        },
+        [3] = {
+            .unk4 = 3,
+        },
+    }
 };
 
 static inline bool8 CheckAxFlag8000(axdata *ptr)
@@ -55,11 +82,208 @@ static inline bool8 ResetFlags(axdata *ptr)
     return ptr->flags = 0;
 }
 
+extern u8 *const gUnknown_80D408C[];
+extern const FileArchive gTitleMenuFileArchive;
+extern const FileArchive gMonsterFileArchive;
+extern const u8 gUnknown_80D40E4[];
+extern const u8 gUnknown_80D40EC[];
 extern const DungeonPos gUnknown_80D40C4[];
 extern const u8 gUnknown_80D40FC[];
 extern const u8 gUnknown_80D4104[];
 extern const u8 gUnknown_80D4120[];
 extern const u8 gUnknown_80D4124[];
+extern const u8 gUnknown_80D40F4[];
+extern const u8 gUnknown_80D407C[];
+extern const u8 gUnknown_80D4080[];
+extern const u8 gUnknown_80D4074[];
+extern const DungeonPos gDungeonCoordinates[];
+
+void sub_8010960(void);
+
+void sub_8010A88(bool8 a0)
+{
+    s32 i;
+    unkStruct_2039DB0 var_2C;
+    DungeonPos pos;
+
+    sub_8004E8C(&var_2C);
+    var_2C.unk4 = 0xF3FF;
+    var_2C.unkA = 0x800;
+    pos = gUnknown_203B0E8->unk52D8;
+    RunAxAnimationFrame(&gUnknown_203B0E8->unk5114);
+    if (a0) {
+        DoAxFrame_800558C(&gUnknown_203B0E8->unk5114, gUnknown_203B0E8->unk52DC - pos.x, gUnknown_203B0E8->unk52DE - pos.y, 3, gUnknown_203B0E8->unk52D4, &var_2C);
+    }
+
+    for (i = 0; i < 64; i++) {
+        struct UnkStruct_Sub1 *structPtr = &gUnknown_203B0E8->unk0[i];
+        if (structPtr->unk0) {
+            axdata *axPtr = &structPtr->unk8;
+
+            RunAxAnimationFrame(axPtr);
+            if (a0) {
+                DoAxFrame_800558C(axPtr, structPtr->unk4.x - pos.x, structPtr->unk4.y - pos.y, 1, 0, &var_2C);
+            }
+        }
+    }
+}
+
+void sub_8010B74(DungeonLocation *dungLocation)
+{
+    const s32 x = 12;
+    UnkTextStruct3 spTxtStruct = {
+        .a0 = {
+            [0] = {
+                .unk4 = 3,
+                .pos = {x, 2},
+                .unkC = 16,
+                .unkE = 2,
+                .unk10 = 2,
+            },
+            [1] = {.unk4 = 3},
+            [2] = {.unk4 = 3},
+            [3] = {.unk4 = 3},
+        }
+    };
+
+    if (gUnknown_203B0E8->unk52DE - gUnknown_203B0E8->unk52D8.y <= 80) {
+        spTxtStruct.a0[0].pos.y = 17;
+    }
+    else {
+        spTxtStruct.a0[0].pos.y = 1;
+    }
+
+    xxx_call_save_unk_text_struct_800641C(&spTxtStruct, TRUE, TRUE);
+    sub_80073B8(0);
+    CopyDungeonName1toBuffer(gFormatBuffer_Monsters[0], dungLocation);
+    PrintFormattedStringOnWindow(12, 2, gUnknown_80D4074, 0, '\0');
+    sub_80073E0(0);
+}
+
+void sub_8010C10(const u8 *text)
+{
+    s32 i, a;
+    CreateMenuDialogueBoxAndPortrait(text, NULL, 0, NULL, NULL, 3, 0, NULL, 0x301);
+    do {
+        DrawDialogueBoxString();
+        sub_8010960();
+    } while (sub_80144A4(&a) != 0);
+
+    for (i = 0; i < 10; i++) {
+        sub_8010960();
+    }
+}
+
+bool8 sub_8010C58(u8 *str)
+{
+    s32 r2, i;
+    MenuInputStruct menuInput;
+
+    r2 = 0;
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == '\n') {
+            r2 += 12;
+        }
+    }
+    r2 += 11;
+    r2 /= 8;
+
+    gUnknown_203B0EC.a0[0].pos.y = 19 - r2;
+    gUnknown_203B0EC.a0[0].unk10 = r2;
+    gUnknown_203B0EC.a0[0].unkE = r2;
+    gUnknown_203B0EC.a0[1].pos.y = 14 - r2;
+    xxx_call_save_unk_text_struct_800641C(&gUnknown_203B0EC, TRUE, TRUE);
+    sub_80073B8(0);
+    PrintFormattedStringOnWindow(4, 0, str, 0, '\0');
+    sub_80073E0(0);
+    sub_80073B8(1);
+    PrintFormattedStringOnWindow(10, 0, gUnknown_80D407C, 1, '\0');
+    PrintFormattedStringOnWindow(10, 12, gUnknown_80D4080, 1, '\0');
+    sub_80073E0(1);
+
+    menuInput.menuIndex = 1;
+    menuInput.unk1A = 2;
+    menuInput.unk1C = 2;
+    menuInput.unk1E = 0;
+    menuInput.unk20 = 0;
+    menuInput.unk4 = 0;
+    menuInput.unk6 = 0;
+    menuInput.unk0 = 1;
+    menuInput.unkC = 0;
+    menuInput.unkE = 0;
+    menuInput.unk14.x = 0;
+    menuInput.unk14.y = 0;
+    menuInput.unk8.x = 8;
+    menuInput.unk8.y = 8;
+    sub_80137B0(&menuInput, 24);
+    sub_801317C(&menuInput.unk28);
+    while (1) {
+        AddMenuCursorSprite(&menuInput);
+        sub_8010960();
+        if (gRealInputs.repeated & DPAD_DOWN) {
+            MoveMenuCursorDown(&menuInput);
+            PlayCursorUpDownSoundEffect();
+        }
+        if (gRealInputs.repeated & DPAD_UP) {
+            MoveMenuCursorUp(&menuInput);
+            PlayCursorUpDownSoundEffect();
+        }
+        if ((gRealInputs.pressed & A_BUTTON) || menuInput.unk28.a_button) {
+            PlayAcceptSoundEffect();
+            break;
+        }
+    }
+
+    if (menuInput.menuIndex == 0) {
+        return TRUE;
+    }
+    else {
+        return FALSE;
+    }
+}
+
+void sub_8010D8C(u8 id, DungeonPos *pos)
+{
+    pos->x = gDungeonCoordinates[id].x;
+    pos->y = gDungeonCoordinates[id].y;
+}
+
+void sub_8010DA4(void)
+{
+    u8 filename[0xC];
+    s32 i, size;
+
+    PokemonStruct1 *pokeStruct = GetPlayerPokemonStruct();
+    OpenedFile *file = OpenFileAndGetFileDataPtr(gUnknown_80D408C[0], &gTitleMenuFileArchive);
+    OpenedFile *file2 = OpenFileAndGetFileDataPtr(gUnknown_80D408C[1], &gTitleMenuFileArchive);
+
+    sprintf(filename, gUnknown_80D40E4, pokeStruct->speciesNum);
+    gUnknown_203B0E4->unk0[3] = OpenFileAndGetFileDataPtr(filename, &gMonsterFileArchive);
+    gUnknown_203B0E4->unk0[2] = OpenFileAndGetFileDataPtr(gUnknown_80D40EC, &gTitleMenuFileArchive);
+    gUnknown_203B0E4->unk0[4] = OpenFileAndGetFileDataPtr(gUnknown_80D408C[2], &gTitleMenuFileArchive);
+    gUnknown_203B0E4->unk0[1] = OpenFileAndGetFileDataPtr(gUnknown_80D40F4, &gMonsterFileArchive);
+    gUnknown_203B0E4->unk0[0] = OpenFileAndGetFileDataPtr(gUnknown_80D408C[3], &gTitleMenuFileArchive);
+    gUnknown_203B0E4->unk4DD0 = GetPokemonOverworldPalette(pokeStruct->speciesNum, 0);
+
+    sub_8005610(gUnknown_203B0E4->unk0[2], 0x40, 0x1F, 0);
+
+    DecompressATGlobalFile((u32 *)(VRAM + 0x8000), 0x0, file);
+    sub_8004AA4(gUnknown_203B0E4->unk4C4C, gUnknown_203B0E4->unk0[4], 0x10);
+    size = 0x4000;
+    DecompressATFile((u8 *) &gUnknown_203B0E4->unk14, size, file2);
+
+    gUnknown_203B0E4->unk4DCC = 0;
+    gUnknown_203B0E4->unk4DD4.x = 0;
+    gUnknown_203B0E4->unk4DD4.y = 0;
+
+    for (i = 0; i < 8; i++) {
+        ResetFlags(&gUnknown_203B0E4->unk4A30[i]);
+    }
+
+    CloseFile(file);
+    CloseFile(file2);
+    xxx_call_save_unk_text_struct_800641C(NULL, 1, 1);
+}
 
 void sub_8010EF0(void)
 {
