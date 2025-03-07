6602a686ba58b983068f34a793c62ed95faa4b0b
DizzyEggg <jajkodizzy@wp.pl>
2025-03-07 21:43:57 +0100

    sub_8040C4C

--- src/code_8040094_1.c
+++ src/code_8040094_1.c
@@ -28,8 +28,8 @@ extern s32 sub_800E710(s32 a0, u16 a1);
 extern void sub_800569C(DungeonPos *, axdata *, u8);
 extern void sub_800EEC8(u16 r0);
 extern void sub_800EF64(void);
-extern s32 sub_800EA84(s32 *param_1);
 extern void sub_8042DD4(s32 a0, Entity *a1, s32 a2);
+extern void sub_803E46C(u32);
 
 u16 sub_80412E0(u16 moveId, u8 weather, bool32 a2);
 bool32 sub_804143C(Entity *entity, Move *move);
@@ -56,22 +56,78 @@ bool8 sub_8040BB0(Entity *entity, Move *move, bool8 a2)
     return r9;
 }
 
-/*
+struct UnkStruct_202EDE8
+{
+    u16 unk0;
+    u16 unk2;
+    DungeonPos unk4;
+    DungeonPos unk8;
+    s32 unkC;
+    s32 unk10;
+};
+
+void sub_8041500(struct UnkStruct_202EDE8 *a0);
+extern s32 sub_800EA84(struct UnkStruct_202EDE8 *a0);
+extern bool8 sub_800E9A8(s32 a0);
+extern void sub_803EA10(void);
+
+extern u8 gUnknown_203B40D;
+extern s16 gUnknown_2026E4E;
+extern s32 gDungeonBrightness;
+
 void sub_8040C4C(Entity *entity, Move *move, bool32 a2)
 {
-    DungeonPos pos;
-    s32 r9 = sub_80412E0(move->id, GetApparentWeather(entity), a2);
+    u8 savedUnkVar;
+    s32 r4;
+    struct UnkStruct_202EDE8 sp;
+    u16 r9 = sub_80412E0(move->id, GetApparentWeather(entity), a2);
     EntityInfo *entInfo = GetEntInfo(entity);
     bool8 r8 = sub_800EC94(move->id);
     s32 apparentId = entInfo->apparentID;
     s32 r2 = sub_800E710(apparentId, sub_80412E0(move->id, GetApparentWeather(entity), TRUE));
 
     if (r2 != -1) {
-        sub_800569C(&pos, &entity->axObj.axdata, r2);
+        sub_800569C(&sp.unk8, &entity->axObj.axdata, r2);
     }
     else {
-        pos = (DungeonPos) {0};
+        sp.unk8 = (DungeonPos) {0};
     }
+
+    sp.unk0 = r9;
+    sp.unk2 = entInfo->apparentID;
+    sp.unk4.x = entity->pixelPos.x / 256;
+    sp.unk4.y = entity->pixelPos.y / 256;
+    sp.unkC = entInfo->action.direction;
+    sp.unk10 = 0;
+    sub_8041500(&sp);
+    sub_800EEC8(r9);
+    sub_800EF64();
+    sub_803E46C(0x58);
+    r4 = sub_800EA84(&sp);
+    sub_8042DD4(r4, entity, 5);
+    savedUnkVar = gUnknown_203B40D;
+    while (1) {
+        if (!sub_800E9A8(r4))
+            break;
+        if (!r8)
+            break;
+
+        gUnknown_203B40D = 1;
+        if (gUnknown_2026E4E != 0x1010) {
+            gUnknown_2026E4E += 0x101;
+        }
+
+        if (gDungeonBrightness >= 5) {
+            gDungeonBrightness -= 4;
+        }
+        else {
+            gDungeonBrightness = 0;
+            r8 = FALSE;
+        }
+        sub_803EA10();
+        sub_803E46C(0x26);
+    }
+    gUnknown_203B40D = savedUnkVar;
 }
-*/
+
 //
