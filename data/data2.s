883d338b99103147b5b176e41507cf32ee53bb91
DizzyEggg <jajkodizzy@wp.pl>
2025-03-14 14:22:46 +0100

    Move InitGraphics from text

--- data/data2.s
+++ data/data2.s
@@ -1,31 +1,5 @@
 	.section .rodata
 
-
-
-@ text.s
-.string "pksdir0\0"
-        .align 2,0
-
-	.global gUnknown_80B88B0
-gUnknown_80B88B0: @ 80B88B0
-        .string "font\0"
-        .align 2,0
-
-	.global gUnknown_80B88B8
-gUnknown_80B88B8: @ 80B88B8
-        .string "fontsp\0"
-        .align 2,0
-
-	.global gUnknown_80B88C0
-gUnknown_80B88C0: @ 80B88C0
-        .string "fontsppa\0"
-        .align 2,0
-
-	.global gUnknown_80B88CC
-gUnknown_80B88CC: @ 80B88CC
-        .string "fontpal\0"
-        .align 2,0
-
-@ code_8009804.s
+@ ?
 .string "pksdir0\0"
 
