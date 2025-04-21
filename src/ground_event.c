4855d0f4c59030ceff6f71f44ddecef5c0bb9624
Seth Barberee <seth.barberee@gmail.com>
2025-04-21 14:50:42 -0700

    more decomp on ground object and event

--- src/ground_event.c
+++ src/ground_event.c
@@ -1,3 +1,51 @@
 #include "global.h"
+#include "memory.h"
 
-IWRAM_INIT void *gGroundEvents = NULL;
+struct GroundEvent_sub
+{
+    s16 unk0;
+    s16 unk2;
+    u8 unk4[0x20 - 0x4];
+};
+
+struct GroundEvents
+{
+    // size: 0x400
+    struct GroundEvent_sub arr[0x20];
+};
+
+IWRAM_INIT struct GroundEvents *gGroundEvents = NULL;
+
+void DeleteGroundEvents(void);
+void GroundEvent_Delete(s32);
+
+void AllocGroundEvents(void) {
+    s32 ind;
+    struct GroundEvent_sub *ptr;
+    gGroundEvents = MemoryAlloc(sizeof(struct GroundEvents), 0x6);
+
+    for(ind = 0, ptr = &gGroundEvents->arr[ind]; ind < 0x20; ind = (s16)(ind + 1), ptr++)
+    {
+        ptr->unk2 |= -1;
+    }
+    DeleteGroundEvents();
+}
+
+void DeleteGroundEvents() {
+    s32 v1;
+    struct GroundEvent_sub* current;
+    
+    current = &gGroundEvents->arr[0];
+    for (v1 = 0; v1 < 0x20; v1 = (s16) (v1 + 0x1), current++) {    
+        if(current->unk2 != -1)
+            GroundEvent_Delete(v1);
+    }
+}
+
+void FreeGroundEvents(void)
+{
+    DeleteGroundEvents();
+    MemoryFree(gGroundEvents);
+    gGroundEvents = NULL;
+}
+
--- src/ground_object.c
+++ src/ground_object.c
@@ -1,3 +1,49 @@
 #include "global.h"
+#include "memory.h"
 
-IWRAM_INIT void *gGroundObjects = NULL;
+typedef struct GroundObject {
+    // size: 0x1C4
+    u8 unk0[6];
+    s16 unk6;
+    u8 fill8[0x1C4 - 8];
+} GroundObject;
+
+IWRAM_INIT GroundObject *gGroundObjects = NULL;
+
+void DeleteGroundObjects(void);
+void GroundObject_Delete(s32);
+
+void AllocGroundObjects(void) 
+{
+
+    GroundObject *ptr;
+    s32 index;
+
+    gGroundObjects = MemoryAlloc(0x1C40, 6);
+   
+    for(index = 0,  ptr = &gGroundObjects[index]; index < 0x10; index = (s16)(index + 1), ptr++)
+    {
+        ptr->unk6 |= -1;
+    }
+    DeleteGroundObjects();
+}
+
+void DeleteGroundObjects(void)
+{
+    GroundObject *ptr;
+    s32 index;
+    
+    ptr = &gGroundObjects[0];
+    for(index = 0; index < 0x10; index = (s16)(index + 1), ptr++)
+    {
+        if(ptr->unk6 != -1)
+            GroundObject_Delete(index);
+    }
+}
+
+void FreeGroundObjects(void)
+{
+    DeleteGroundObjects();
+    MemoryFree(gGroundObjects);
+    gGroundObjects = NULL;
+}
