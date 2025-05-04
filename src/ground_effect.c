495a95e81bd3170e535561de6a8e6377ccfd79a5
Seth Barberee <seth.barberee@gmail.com>
2025-05-03 18:37:50 -0700

    decomp more of the ground select funcs

--- src/ground_effect.c
+++ src/ground_effect.c
@@ -1,24 +1,33 @@
 #include "global.h"
+#include "ground_script.h"
+#include "debug.h"
 #include "memory.h"
 
 void DeleteGroundEffects(void);
 
 // size must be 0x1c4
-struct unkStruct_3001B8C {
+typedef struct GroundEffect {
     u8 fill0[6];
     s16 unk6;
-    u8 fill8[0x1c4 - 8];
-};
+    s16 unk8;
+    s8 unkA;
+    u8 fillB[0x1c4 - 0xB];
+} GroundEffect;
 
-IWRAM_INIT struct unkStruct_3001B8C* gGroundEffects = NULL;  // size 16 array
+IWRAM_INIT GroundEffect* gGroundEffects = NULL;  // size 16 array
 void GroundEffect_Delete(s32);
+const struct GroundScriptHeader *GetGroundScript(s16 a0, DebugLocation *);
+extern DebugLocation gUnknown_8118610;
+extern u8 gUnknown_811861C[];
+extern u8 gUnknown_8118640[];
+s16 GroundEffect_Add(s32 id, const GroundEffectData*, s32 group, s32 sector);
 
 void AllocGroundEffects()
 {
     s32 index;
-    struct unkStruct_3001B8C* current;
+    GroundEffect* current;
     
-    gGroundEffects = MemoryAlloc(16 * sizeof(struct unkStruct_3001B8C), 6);
+    gGroundEffects = MemoryAlloc(16 * sizeof(GroundEffect), 6);
     current = gGroundEffects;
     for (index = 0; index < 16; index = (s16) (index + 0x1), current++) {    
         current->unk6 = -1;
@@ -30,7 +39,7 @@ void AllocGroundEffects()
 void DeleteGroundEffects(void)
 {
     s32 index;
-    struct unkStruct_3001B8C* current;
+    GroundEffect* current;
     
     current = gGroundEffects;
     for (index = 0; index < 16; index = (s16) (index + 0x1), current++) {    
@@ -45,3 +54,57 @@ void FreeGroundEffects(void)
     MemoryFree(gGroundEffects);
     gGroundEffects = NULL;
 }
+
+void GroundEffect_Select(s32 scriptID, s32 group, s32 sector)
+{
+    const struct GroundScriptHeader *scriptPtr;
+    const struct GroundScriptSector *sectorPtr;
+    const struct GroundScriptGroup *groupPtr;
+
+    s32 i;
+    s32 size;
+    const GroundEffectData *effectData;
+    s32 sector_s32;
+    s32 group_s32;
+    s32 scriptID_s32;
+
+    scriptID_s32 = (s16)scriptID;
+    group_s32 = (s16)group;
+    sector_s32 = (s8)sector;
+    scriptPtr = GetGroundScript(scriptID_s32, &gUnknown_8118610);
+    Log('\0',gUnknown_811861C, scriptID_s32, group_s32, sector_s32);
+
+    groupPtr = &scriptPtr->groups[group_s32];
+    sectorPtr = &groupPtr->sectors[sector_s32];
+    effectData = sectorPtr->effects;
+
+    size = sectorPtr->nEffects;
+    for (i = 0; i < size; i++, effectData++)
+    {
+        GroundEffect_Add(-1,effectData,group_s32,sector_s32);
+    }
+}
+
+void GroundEffect_Cancel(s32 scriptID, s32 sector)
+{
+    s32 index;
+    GroundEffect *ptr;
+    s32 sector_s32;
+    s32 scriptID_s32;
+
+    scriptID_s32 = (s16)scriptID;
+    sector_s32 = (s8) sector;
+
+
+    Log('\0',gUnknown_8118640, scriptID_s32);
+
+    index = 0;
+    ptr = &gGroundEffects[0];
+    for(; index < 0x10; index = (s16)(index + 1), ptr++)
+    {
+        if((ptr->unk6 != -1) && (ptr->unk8 == scriptID_s32))
+            if(sector_s32 < 0 || ptr->unkA == sector_s32)
+                GroundEffect_Delete(index);
+    }
+}
+
--- src/ground_lives.c
+++ src/ground_lives.c
@@ -26,7 +26,9 @@ struct unkStruct_3001B84_sub
     // size: 0x1F0
     u16 unk0;
     s16 unk2;
-    u8 unk4[0x1F0 - 0x4];
+    s16 unk4;
+    s8 unk6;
+    u8 unk8[0x1F0 - 0x8];
 };
 
 struct unkStruct_3001B84
@@ -38,6 +40,11 @@ IWRAM_INIT struct unkStruct_3001B84 *gGroundLives = NULL;
 
 extern void DeleteGroundLives(void);
 void GroundLives_Delete(s32);
+s16 GroundLives_Add(s32 id, const GroundLivesData*, s32 group, s32 sector);
+const struct GroundScriptHeader *GetGroundScript(s16 a0, DebugLocation *);
+extern DebugLocation gUnknown_811808C;
+extern u8 gUnknown_8118098[];
+extern u8 gUnknown_81180BC[];
 
 void AllocGroundLives(void)
 {
@@ -101,4 +108,57 @@ void FreeGroundLives(void)
     gGroundLives = NULL;
     MemoryFree(gGroundLivesMeta);
     gGroundLivesMeta = NULL;
+}
+
+void GroundLives_Select(s32 scriptID, s32 group, s32 sector)
+{
+    const struct GroundScriptHeader *scriptPtr;
+    const struct GroundScriptSector *sectorPtr;
+    const struct GroundScriptGroup *groupPtr;
+
+    s32 i;
+    s32 size;
+    const GroundLivesData *livesData;
+    s32 sector_s32;
+    s32 group_s32;
+    s32 scriptID_s32;
+
+    scriptID_s32 = (s16)scriptID;
+    group_s32 = (s16)group;
+    sector_s32 = (s8)sector;
+    scriptPtr = GetGroundScript(scriptID_s32, &gUnknown_811808C);
+    Log('\0',gUnknown_8118098, scriptID_s32, group_s32, sector_s32);
+
+    groupPtr = &scriptPtr->groups[group_s32];
+    sectorPtr = &groupPtr->sectors[sector_s32];
+    livesData = sectorPtr->lives;
+
+    size = sectorPtr->nLives;
+    for (i = 0; i < size; i++, livesData++)
+    {
+        GroundLives_Add(-1,livesData,group_s32,sector_s32);
+    }
+}
+
+void GroundLives_Cancel(s32 scriptID, s32 sector)
+{
+    s32 index;
+    struct unkStruct_3001B84_sub *ptr;
+    s32 sector_s32;
+    s32 scriptID_s32;
+
+    scriptID_s32 = (s16)scriptID;
+    sector_s32 = (s8) sector;
+
+
+    Log('\0',gUnknown_81180BC, scriptID_s32, sector_s32);
+
+    index = 0;
+    ptr = &gGroundLives->array[0];
+    for(; index < 0x18; index = (s16)(index + 1), ptr++)
+    {
+        if((ptr->unk2 != -1) && (ptr->unk4 == scriptID_s32))
+            if(sector_s32 < 0 || ptr->unk6 == sector_s32)
+                GroundLives_Delete(index);
+    }
 }
