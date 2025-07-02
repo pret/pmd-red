5c1ac3892f9f96802d832a81e9e6436892a3d4db
DizzyEggg <jajkodizzy@wp.pl>
2025-07-02 10:26:56 +0200

    start ornament work

---
+++ include/ornament_files_table.h
@@ -0,0 +1,10 @@
+#ifndef GUARD_ORNAMENT_FILES_TABLE_H
+#define GUARD_ORNAMENT_FILES_TABLE_H
+
+#define ORNAMENT_FILES_COUNT 75
+
+#include "structs/str_file_system.h"
+
+extern const File gOrnamentFiles[ORNAMENT_FILES_COUNT];
+
+#endif // GUARD_ORNAMENT_FILES_TABLE_H
