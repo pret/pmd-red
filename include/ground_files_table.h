77bf1d08eaff73199d81c4f0df5f78653450c9e7
DizzyEggg <jajkodizzy@wp.pl>
2025-07-04 13:00:06 +0200

    ground files table

---
+++ include/ground_files_table.h
@@ -0,0 +1,10 @@
+#ifndef GUARD_GROUND_FILES_TABLE_H
+#define GUARD_GROUND_FILES_TABLE_H
+
+#define GROUND_FILES_COUNT 677
+
+#include "structs/str_file_system.h"
+
+extern const File gGroundFiles[GROUND_FILES_COUNT];
+
+#endif // GUARD_GROUND_FILES_TABLE_H
